c87f60c9506a7f96792e937d08cca35c1d649ce3
unknown <AbrarMAlhillawi@gmail.com>
2022-11-03 16:47:09 +0200

    new tasks

---
+++ wednesday/task3.js
@@ -0,0 +1,26 @@
+mark=prompt("enter your mark !");
+parseInt(mark);
+if(mark < 50) 
+{
+    alert("Fail !!");
+}
+else if(50 < mark <59) {
+     alert("D");
+}
+  else if(60 < mark <69) {
+   alert("C");
+}
+   else if(70 < mark<79)
+{
+    alert("B");
+}
+   else if(80 < mark <90)
+{ 
+    alert("A");
+
+}
+    else if(90< mark <100)
+{
+    alert("A+");
+}
+
