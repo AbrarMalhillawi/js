4b1fdce9d13e742ceddcda8e6b90806fa51be9c0
unknown <AbrarMAlhillawi@gmail.com>
2022-11-06 15:27:24 +0200

    tasks

--- sunday/task1.js
+++ sunday/task1.js
@@ -2,25 +2,44 @@ let month=prompt("enter your month?");
 switch(month)
 {
     case "1":
-        console.log("January");
+       alert("January");
         break;
          
     case "2": 
-    console.log("february");
-    break;
+        alert("february");
+         break;
 
     case "3":
-     console.log("march");
-     break;
-    case "4": 
-    console.log("april");
-    break; 
-    case "5": 
-    console.log("may");
-    break;
-    case "6" :
-        console.log("june");
+        alert("march");
         break;
+    case "4": 
+        alert("april");
+        break; 
+    case "5": 
+        alert("may");
+        break;
+    case "6" :
+        alert("june");
+        break;
+    case "7":
+        alert("july");
+        break;
+    case "8":;  
+        alert("august");
+        break;   
+    case "9":
+        alert("september");
+        break;
+    case "10":
+        alert("october");
+        break;
+    case "11":
+        alert("november");
+        break;
+    case "12": 
+         alert("december");
+         break;
+
     default:
         alert("not found!!");     
 }
