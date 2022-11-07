a5c938d8833eec2e59fbecf86610dac280fd9fe7
unknown <AbrarMAlhillawi@gmail.com>
2022-11-07 22:35:27 +0200

    task

--- test/test1.js
+++ test/test1.js
@@ -71,14 +71,112 @@
 
 
 
+// const array=["a","b","d",];
+// let x;
+//  for( x=0 ;x<3 ; x++ )
+//  { 
+//     document.write(array[6]+'<br>');
+//  }
 
 
-const array=["a","b","d",];
-let x;
- for( x=0 ;x<3 ; x++ )
- { 
-    document.write(array[6]+'<br>');
- }
+// let x=prompt("enter the number of group:");
+
+//  if(x == '2')
+//  {
+//    document.write("groub 2");
+//  }
+//  else if(x == '1')
+// {
+//    document.write("groub 1");  
+// }
+
+// else if (x=='3')
+// {
+//    document.write("groub 3");
+// }
+
+
+// else 
+// {
+//    document.write("not found groub ");
+// }
+
+
+// let x=3;
+
+// switch(x)
+// {
+// case 3:
+//    {
+//       document.write("the string a");
+      
+//    }
+// case 5:
+//    {
+//        document.write("the string b");
+//        breck;
+//    }
+//    case 0:
+//       {
+//           document.write("the string c");
+//           breck;
+//       }  
+// default :
+// {
+//    document.write("not found");
+// }      
+
+// }
+
+
+// loop {1.for   2.while   3. do while }
+// to print number from 1 to 1000
+
+// let x;
+// for( x=1  ;x<=1000    ;  x++  )
+// {   if(x % 13 ==0 ){
+//     document.write(x);
+//     document.write("<br>");
+// }
+// }
+
+
+// let z=1;
+//    do
+// {
+//    document.write(z);
+//    document.write('<br>');
+//    z++;
+// }while(z<6)
+
+//   document.write("go back");
+
+// let z=1;
+// for( let x=1; x<3 ;x++ )
+// {
+//    for(let y=1 ;y<3 ;y++)
+//    {
+//        document.write(z);
+//        z++;
+        
+//     }
+//    document.write("<br>");
+// }
+let ahmed,omer,mohammed;
+ahmed=80;
+omer=90;
+mohammed=80;
+ avareg(ahmed,omer,mohammed);
+
+
+function avareg(num1,num2,num3)
+{
+   let result;
+   result=(num1+num2+num3)/3;
+   document.write(result);
+}
+
+
 
 
  
